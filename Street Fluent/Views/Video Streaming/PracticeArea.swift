66df97cb3dc6fd1045e70e148fd221e4fd98dfbd
the-netizen <khanaima020815@gmail.com>
2026-03-02 08:38:25 +0300

    started working on video streaming

---
+++ Street Fluent/Views/Video Streaming/PracticeArea.swift
@@ -0,0 +1,18 @@
+//
+//  PracticeArea.swift
+//  Street Fluent
+//
+//  Created by Naima Khan on 02/03/2026.
+//
+
+import SwiftUI
+
+struct PracticeArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PracticeArea()
+}
--- Street Fluent/Views/Video Streaming/VideoStreaming.swift
+++ Street Fluent/Views/Video Streaming/VideoStreaming.swift
@@ -3,7 +3,31 @@ import SwiftUI
 
 struct VideoStreaming: View {
     var body: some View {
-        Text("Video Streaming Page!")
+
+        VStack(spacing: 20) {
+            //video streaming area
+            VStack{
+                ZStack{
+                    //video
+                    //progress bar of video
+                }
+                
+                //interactive subtitles
+                
+                //translation
+            }
+            
+            //practice area
+            VStack{
+                // controls -
+                // if speaking mode -> replay dialogue button and , skip dialogue button
+                // if writing mode -> toggle visibility (of writing help) button, skip dialogue button
+                
+                //learning area -
+                // if speaking mode -> show big mic button
+                // if writing mode -> show writing canvas
+            }
+        }
     }
 }
 
